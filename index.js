02dfaba286c15ece3a620c952cbcf438979a5844
Velospace <86271580+Velospace@users.noreply.github.com>
2024-10-20 18:37:00 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
+<script>
+    let count = 0;
 
+    function increment() {
+        count++;
+        console.log(count);
+        document.getElementById("count-el").innerText = count;
+    }
+</script>
