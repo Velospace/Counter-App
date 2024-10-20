397831480177c97fb37c057ab80a3f8bea882b70
Velospace <86271580+Velospace@users.noreply.github.com>
2024-10-20 18:39:53 -0400

    Delete index.js

--- index.js
+++
@@ -1,9 +0,0 @@
-<script>
-    let count = 0;
-
-    function increment() {
-        count++;
-        console.log(count);
-        document.getElementById("count-el").innerText = count;
-    }
-</script>
